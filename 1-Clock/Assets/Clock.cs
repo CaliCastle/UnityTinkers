91b7b92495726724d1643906b6e47a363194e6c7
Cali Castle <cali@calicastle.com>
2018-11-29 00:41:52 -0800

    Add 1-clock

---
+++ 1-Clock/Assets/Clock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+	[SerializeField] private Transform m_hoursTransform, m_minutesTransform, m_secondsTransform;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var time = System.DateTime.Now.TimeOfDay;
+		m_hoursTransform.localRotation =
+			Quaternion.Euler(0f, (float) time.TotalHours * 30f, 0f);
+		m_minutesTransform.localRotation =
+			Quaternion.Euler(0f, (float) time.TotalMinutes * 6f, 0f);
+		m_secondsTransform.localRotation =
+			Quaternion.Euler(0f, (float) time.TotalSeconds * 6f, 0f);
+	}
+}
